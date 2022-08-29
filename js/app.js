3613280fe4bf2eb0a6ce16bbe8928b2e7284abcf
jonathan-crepeau <jon.crepeau@gmail.com>
2022-08-29 13:11:43 -0700

    game object created and square color function complete

--- js/app.js
+++ js/app.js
@@ -1,12 +1,37 @@
 // console.log('Fear is the mind killer.');
 
 $('.start-btn').on('click', () => {
-    createSquares(20);
+    gameObject.createSquares(20);
 })
 
-function createSquares(numberOfSquares) {
-    for (let i = 0; i < numberOfSquares; i++) {
-        const newSquare = $('<div class="game-square"></div>');
-        $('.squares-container').append(newSquare);
+const gameObject = {
+    time: 10,
+    rounds: 1,
+    score: 0,
+    playGame() {
+        if (gameObject.rounds >= 4) {
+            return alert('Game over!');
+        } else {
+            console.log(gameObject.rounds);
+            gameObject.rounds++
+            this.playGame()
+        }
+    },
+    createSquares(numberOfSquares) {
+        for (let i = 0; i < numberOfSquares; i++) {
+            const newSquare = $('<div class="game-square"></div>');
+            gameObject.colorSquares(newSquare);
+            $('.squares-container').append(newSquare);
+        }
+    },
+    colorSquares(newSquare) {
+        const num = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+        if (num === 1) {
+            $(newSquare).css('background', 'red');
+        } else if (num === 2) {
+            $(newSquare).css('background', 'blue');
+        } else {
+            $(newSquare).css('background', 'green');
+        }
     }
 }
