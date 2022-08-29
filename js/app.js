3512b85466f84925464ebeed8e24b91d35aeb4b8
jonathan-crepeau <jon.crepeau@gmail.com>
2022-08-29 12:52:48 -0700

    functions to create squares and add btn click event complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log('Fear is the mind killer.');
+// console.log('Fear is the mind killer.');
+
+$('.start-btn').on('click', () => {
+    createSquares(20);
+})
+
+function createSquares(numberOfSquares) {
+    for (let i = 0; i < numberOfSquares; i++) {
+        const newSquare = $('<div class="game-square"></div>');
+        $('.squares-container').append(newSquare);
+    }
+}
