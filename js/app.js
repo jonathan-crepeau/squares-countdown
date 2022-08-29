34056011c5503136dca2000df858d830d47f324e
jonathan-crepeau <jon.crepeau@gmail.com>
2022-08-29 13:28:51 -0700

    handle game square click function v1 & update score function complete

--- js/app.js
+++ js/app.js
@@ -2,7 +2,10 @@
 
 $('.start-btn').on('click', () => {
     gameObject.createSquares(20);
-})
+    $('.game-square').on('click', gameObject.handleClick);
+});
+
+
 
 const gameObject = {
     time: 10,
@@ -27,11 +30,31 @@ const gameObject = {
     colorSquares(newSquare) {
         const num = Math.floor(Math.random() * (3 - 1 + 1) + 1);
         if (num === 1) {
-            $(newSquare).css('background', 'red');
+            $(newSquare).css('background-color', 'red');
         } else if (num === 2) {
-            $(newSquare).css('background', 'blue');
+            $(newSquare).css('background-color', 'blue');
         } else {
-            $(newSquare).css('background', 'green');
+            $(newSquare).css('background-color', 'green');
         }
-    }
+    },
+    handleClick(event) {
+        const squareColor = $(event.target).css("background-color");
+        const thirdNum = squareColor.substring(4, squareColor.length - 1).split(" ");
+        if (thirdNum[2] === '255') {
+            gameObject.updateScore('win');
+            $(event.target).remove();
+        } else {
+            gameObject.updateScore('loss');
+            $(event.target).remove();
+        }
+    },
+    updateScore(input) {
+        if (input === 'win') {
+            gameObject.score++;
+            $('.scoreboard').html('scoreboard ' + gameObject.score)
+        } else {
+            gameObject.score--;
+            $('.scoreboard').html('scoreboard ' + gameObject.score)
+        }
+    },
 }
